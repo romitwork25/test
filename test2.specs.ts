783abc3589e97a15ffe407284636fc5f07eb3dfe
romitwork25 <romitwork25@gmail.com>
2024-08-14 18:07:15 +0530

    Create test2.specs.ts

---
+++ test2.specs.ts
@@ -0,0 +1,4 @@
+console.log('updateColDefItem called:', service.updateColDefItem.mock.calls.length > 0);
+  console.log('updateColDefHeader called:', service.updateColDefHeader.mock.calls.length > 0);
+  console.log('updateColDefHeaderTooltip called:', service.updateColDefHeaderTooltip.mock.calls.length > 0);
+  console.log('updateColDefCellRenderer called:', service.updateColDefCellRenderer.mock.calls.length > 0);
