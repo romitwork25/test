a46df3ddb70bc8a1de109b0f09ae4ce6265347e7
romitwork25 <romitwork25@gmail.com>
2024-08-16 15:02:02 +0530

    Update form-processing.spec.ts

--- form-processing.spec.ts
+++ form-processing.spec.ts
@@ -47,3 +47,79 @@ it('populateDomainFromForm should mark control as touched if it has errors', () 
     });
   });
 });
+
+
+
+// ---------------------------- second
+it('populateDomainFromForm with form errors and array field', () => {
+  jest.spyOn(service.dataService, 'getFormList').mockReturnValue([
+    new FormGroup({
+      formField: new FormControl('formFieldValue', { validators: [Validators.required] }), // Validator to simulate error
+      arrayField: new FormControl(['arrayFieldValue']),
+      dateField: new FormControl(new Date()),
+      invalidField: new FormControl({}, { validators: [Validators.required] }), // Error case
+      refKeyField: new FormControl({
+        refkey: 'refValue'
+      })
+    })
+  ]);
+
+  let customerState: any = {
+    path: {
+      formField: null
+    },
+    arrayPath: {
+      arrayField: []
+    },
+    invalidPath: {
+      invalidField: undefined
+    },
+    datePath: {
+      dateField: null
+    },
+    refKeyPath: {
+      customField: ''
+    }
+  };
+
+  jest.spyOn(service.dataService, 'getFieldMap').mockReturnValue({
+    formField: {
+      path: 'path',
+      refkey: 'refkey',
+      datakey: 'formField'
+    },
+    arrayField: {
+      path: 'arrayPath',
+      refkey: 'refkey',
+      datakey: 'arrayField'
+    },
+    dateField: {
+      path: 'datePath'
+    },
+    invalidField: {
+      path: 'invalidPath',
+      refKey: 'refKey',
+      datakey: 'invalidField'
+    },
+    refKeyField: {
+      path: 'refKeyPath',
+      refKey: 'refKey'
+    }
+  });
+
+  // Call the function
+  service.populateDomainFromForm(customerState);
+
+  setTimeout(() => {
+    // Expect formField to be updated
+    expect(customerState.path.formField).toEqual('formFieldValue');
+    
+    // Expect arrayField to be updated in arrayPath
+    expect(customerState.arrayPath.arrayField).toEqual(['arrayFieldValue']);
+    
+    // Expect form controls to be marked as touched due to errors
+    expect(service.dataService.getFormList()[0].controls['formField'].touched).toBe(true);
+    expect(service.dataService.getFormList()[0].controls['invalidField'].touched).toBe(true);
+  });
+});
+
