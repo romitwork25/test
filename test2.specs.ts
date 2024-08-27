d7a289077cae1c419e849f2ca579764a0c98323f
romitwork25 <romitwork25@gmail.com>
2024-08-27 11:50:43 +0530

    Update test2.specs.ts

--- test2.specs.ts
+++ test2.specs.ts
@@ -30,3 +30,31 @@ it('should include facilityRuleMessages in payload when present', async () => {
 // 
 // 
 // 
+it('should include empty object for facilityRuleMessages in payload when not present', async () => {
+  const action = {
+    type: FacilityActions.FETCH,
+    payload: {
+      sourceTab: 'HELIOS',
+      destinationTab: 'Helme datatla',
+      facilityData: {
+        test: 'test'
+      }
+    }
+  };
+
+  jest.spyOn(guiHttpHelperService, 'getFacilityRuleMessages').mockReturnValue(
+    of({ facilityRuleMessages: null })
+  );
+  jest.spyOn(guiHttpHelperService, 'getFacilityData').mockReturnValue(
+    of({ facilityRuleMessages: '', status: 'SUCCESS' })
+  );
+
+  facilityEffects.initEffects();
+  const res = await store.dispatch(action);
+
+  expect(res.globalState).toEqual({
+    data: {
+      [serviceConstants.ListofRuleMessages]: {}
+    }
+  });
+});
